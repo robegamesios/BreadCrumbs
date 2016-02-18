6a9ccc068d7fae440881efecd2e1dfa3f87126df
Rob Enriquez <rob@goabra.com>
2016-02-17 21:39:08 -0800

    added MapAnnotation view

---
+++ BreadCrumbs/BreadCrumbs/MapAnnotation.m
@@ -0,0 +1,37 @@
+//
+//  MapAnnotation.m
+//  BreadCrumbs
+//
+//  Created by Rob Enriquez on 2/17/16.
+//  Copyright © 2016 Rob Enriquez. All rights reserved.
+//
+
+#import "MapAnnotation.h"
+
+@implementation MapAnnotation
+
+- (id)initWithCoordinate:(CLLocationCoordinate2D)aCoordinate
+{
+    self = [super init];
+    if (self) {
+        _coordinate = aCoordinate;
+    }
+    return self;
+}
+
+#pragma mark equality
+
+- (BOOL)isEqual:(MapAnnotation*)annotation;
+{
+    if (![annotation isKindOfClass:[MapAnnotation class]]) {
+        return NO;
+    }
+    
+    return (self.coordinate.latitude == annotation.coordinate.latitude &&
+            self.coordinate.longitude == annotation.coordinate.longitude &&
+            [self.title isEqualToString:annotation.title] &&
+            [self.subtitle isEqualToString:annotation.subtitle] &&
+            [self.groupTag isEqualToString:annotation.groupTag]);
+}
+
+@end
